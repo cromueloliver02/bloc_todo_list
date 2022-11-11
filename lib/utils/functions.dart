52393992ed2e850366405d23976e1467492b89f5
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-11 23:29:15 +0800

    applied unfocusing on search input when tapping create todo button

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import '../models/todo.dart';
 import '../components/components.dart';
 
-void showTodoModal(BuildContext ctx, [Todo? todo]) => showModalBottomSheet(
-      context: ctx,
-      isScrollControlled: true,
-      builder: (ctx) => TDLTodoModal(todo: todo),
-    );
+void showTodoModal(BuildContext ctx, [Todo? todo]) {
+  FocusScope.of(ctx).unfocus();
+
+  showModalBottomSheet(
+    context: ctx,
+    isScrollControlled: true,
+    builder: (ctx) => TDLTodoModal(todo: todo),
+  );
+}
 
 Future<bool?> showConfirmDialog(
   BuildContext ctx, {
