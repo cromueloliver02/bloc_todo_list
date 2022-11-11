f6a592ffb823d02baa5aa021c54911139b8017ab
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-11 17:17:16 +0800

    isolated bloc providers from the root file

--- lib/todo_list_app.dart
+++ lib/todo_list_app.dart
@@ -1,44 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import './blocs/blocs.dart';
 import './pages/pages.dart';
+import 'utils/blocs_handler.dart';
 import './utils/routes_handler.dart';
 import './utils/themes_handler.dart';
 
 class TodoListApp extends StatelessWidget {
   TodoListApp({super.key});
 
+  final _blocsHandler = BlocsHandler();
   final _routesHandler = RoutesHandler();
   final _themesHandler = ThemesHandler();
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (ctx) => TodoListBloc()),
-        BlocProvider(create: (ctx) => TodoSearchBloc()),
-        BlocProvider(create: (ctx) => TodoFilterBloc()),
-        BlocProvider(create: (ctx) => TabBloc()),
-        BlocProvider(
-          create: (ctx) => FavoriteTodosBloc(
-            todoListBloc: ctx.read<TodoListBloc>(),
-            initialFavoriteTodos: ctx
-                .read<TodoListBloc>()
-                .state
-                .todos
-                .where((d) => d.isFavorite)
-                .toList(),
-          ),
-        ),
-        BlocProvider(
-          create: (ctx) => FilteredTodosBloc(
-            todoFilterBloc: ctx.read<TodoFilterBloc>(),
-            todoSearchBloc: ctx.read<TodoSearchBloc>(),
-            todoListBloc: ctx.read<TodoListBloc>(),
-            initialFilteredTodos: ctx.read<TodoListBloc>().state.todos,
-          ),
-        ),
-      ],
+      providers: _blocsHandler.blocProviders,
       child: MaterialApp(
         title: 'Todo List',
         debugShowCheckedModeBanner: false,
---
+++ lib/utils/blocs_handler.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../blocs/blocs.dart';
+
+class BlocsHandler {
+  final List<BlocProvider> blocProviders = [
+    BlocProvider<TodoListBloc>(create: (ctx) => TodoListBloc()),
+    BlocProvider<TodoSearchBloc>(create: (ctx) => TodoSearchBloc()),
+    BlocProvider<TodoFilterBloc>(create: (ctx) => TodoFilterBloc()),
+    BlocProvider<TabBloc>(create: (ctx) => TabBloc()),
+    BlocProvider<FavoriteTodosBloc>(
+      create: (ctx) => FavoriteTodosBloc(
+        todoListBloc: ctx.read<TodoListBloc>(),
+        initialFavoriteTodos: ctx
+            .read<TodoListBloc>()
+            .state
+            .todos
+            .where((d) => d.isFavorite)
+            .toList(),
+      ),
+    ),
+    BlocProvider<FilteredTodosBloc>(
+      create: (ctx) => FilteredTodosBloc(
+        todoFilterBloc: ctx.read<TodoFilterBloc>(),
+        todoSearchBloc: ctx.read<TodoSearchBloc>(),
+        todoListBloc: ctx.read<TodoListBloc>(),
+        initialFilteredTodos: ctx.read<TodoListBloc>().state.todos,
+      ),
+    ),
+  ];
+}
